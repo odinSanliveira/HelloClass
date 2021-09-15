c3928b10ea1c95c293aba04d9e856960235efec5
odinSanliveira <odin.sanliveira@gmail.com>
2021-09-15 11:00:06 -0300

    primeira aula .NET

--- HelloClass/Program.cs
+++ HelloClass/Program.cs
@@ -6,7 +6,7 @@ namespace HelloClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá, Odin Santos");
         }
     }
 }
